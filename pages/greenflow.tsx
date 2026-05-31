a306cad0fbe7f5cf3b490d99cfa8c62f28024b9d
arpitkumar28 <arpitkumar0211@gmail.com>
2026-05-31 12:03:56 +0530

    chore: add GreenFlow AI demo

---
+++ pages/greenflow.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import GreenFlowShowcase from '../components/greenflow/GreenFlowShowcase'
+
+export default function GreenflowPage() {
+  return (
+    <main className="min-h-screen bg-[#061225]">
+      <GreenFlowShowcase />
+    </main>
+  )
+}
